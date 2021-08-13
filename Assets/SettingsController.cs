eb1ca94650829ad853a18a1ead717983f56e5ad4
KarlRoeGames <karl.roe2@mail.dcu.ie>
2021-08-13 18:56:11 +0100

    [Game-1] Added a volume panel
    Added a new class for managing the game settings e.g. Volume.
    Added a class on the volume slider that sends the new volume to the SettingsController.
    Added a Volume panel prefab with a slider to adjust the games volume, which is saved to the player prefs.

---
+++ Assets/SettingsController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SettingsController : MonoBehaviourSingleton<SettingsController>
+{
+    private const string VolumePrefsKey = "Volume";
+
+    protected override void Awake()
+    {
+        base.Awake();
+        AudioListener.volume = PlayerPrefs.GetFloat(VolumePrefsKey);
+    }
+
+    public void UpdateVolume(float newVolume)
+    {
+        AudioListener.volume = newVolume;
+        PlayerPrefs.SetFloat(VolumePrefsKey, newVolume);
+    }
+
+    public float GetVolume()
+    {
+        return PlayerPrefs.GetFloat(VolumePrefsKey);
+    }
+}
---
+++ Assets/VolumeSlider.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    private Slider _VolumeSlider;
+
+    private void Awake()
+    {
+        _VolumeSlider = GetComponent<Slider>();
+        if(!_VolumeSlider)
+        {
+            Debug.LogError("VolumeSlider must have a Slider component attached");
+        }
+
+    }
+
+    private void Start()
+    {
+        _VolumeSlider.value = SettingsController.Instance.GetVolume();
+    }
+
+    public void OnVolumeChanged()
+    {
+        SettingsController.Instance.UpdateVolume(_VolumeSlider.value);
+    }
+}
